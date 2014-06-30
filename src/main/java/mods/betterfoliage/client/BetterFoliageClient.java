37ffa219fc16c9afee6e6aa3d4ba7fb1fa377483
octarine-noise <octarine.noise@gmail.com>
2014-06-30 22:15:57 +0200

    Cache leaf block IDs

--- src/main/java/mods/betterfoliage/client/BetterFoliageClient.java
+++ src/main/java/mods/betterfoliage/client/BetterFoliageClient.java
@@ -1,6 +1,7 @@
 package mods.betterfoliage.client;
 
 import java.io.File;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 
@@ -17,18 +18,22 @@ import net.minecraft.block.BlockLeavesBase;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.world.IBlockAccess;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.world.WorldEvent;
 
 import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
 
 import cpw.mods.fml.client.registry.RenderingRegistry;
 import cpw.mods.fml.common.FMLCommonHandler;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 
 public class BetterFoliageClient implements ILeafTextureRecognizer {
 
 	public static Map<Integer, IRenderBlockDecorator> decorators = Maps.newHashMap();
-	public static Set<Class<?>> blockLeavesClasses = Sets.newHashSet();
 	public static LeafTextureGenerator leafGenerator;
+	
+	public static Set<Class<?>> blockLeavesClasses = Sets.newHashSet();
+	public static Set<Integer> leafBlockIDs = Sets.newHashSet();
 	
 	public static void preInit() {
 		FMLCommonHandler.instance().bus().register(new KeyHandler());
@@ -48,6 +53,8 @@ public class BetterFoliageClient implements ILeafTextureRecognizer {
 		MinecraftForge.EVENT_BUS.register(leafGenerator);
 		leafGenerator.recognizers.add(new BetterFoliageClient());
 		leafGenerator.loadLeafMappings(new File(BetterFoliage.configDir, "leafMask.properties"));
+
+		MinecraftForge.EVENT_BUS.register(new BetterFoliageClient());
 	}
 
 	public boolean isLeafTexture(TextureAtlasSprite icon) {
@@ -79,6 +86,22 @@ public class BetterFoliageClient implements ILeafTextureRecognizer {
 		try {
 			blockLeavesClasses.add(Class.forName(className));
 		} catch(ClassNotFoundException e) {
+		}
+	}
+	
+	/** Caches leaf block IDs on world load for fast lookup
+	 * @param event
+	 */
+	@SuppressWarnings("unchecked")
+	@SubscribeEvent
+	public void handleWorldLoad(WorldEvent.Load event) {
+		Iterator<Block> iter = Block.blockRegistry.iterator();
+		while (iter.hasNext()) {
+			Block block = iter.next();
+			int blockId = Block.blockRegistry.getIDForObject(block);
+			for (Class<?> clazz : BetterFoliageClient.blockLeavesClasses)
+				if (clazz.isAssignableFrom(block.getClass()))
+					leafBlockIDs.add(blockId);
 		}
 	}
 }
