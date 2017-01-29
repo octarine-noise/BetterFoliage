eb6058c4eef416ea670cd94db47c5edb82329800
octarine-noise <octarine.noise@gmail.com>
2017-01-29 10:11:53 +0100

    port to MC 1.11.2

--- src/main/kotlin/mods/betterfoliage/loader/BetterFoliageCore.kt
+++ src/main/kotlin/mods/betterfoliage/loader/BetterFoliageCore.kt
@@ -13,7 +13,7 @@ import org.objectweb.asm.Opcodes.*
     "kotlin",
     "mods.octarinecore.kotlin"
 )
-@IFMLLoadingPlugin.MCVersion("1.11")
+@IFMLLoadingPlugin.MCVersion("1.11.2")
 class BetterFoliageLoader : ASMPlugin(BetterFoliageTransformer::class.java)
 
 class BetterFoliageTransformer : Transformer() {
