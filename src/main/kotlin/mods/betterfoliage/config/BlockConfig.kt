594db19bfbb710df2a84727ac2a652ace52b65e5
octarine-noise <octarine.noise@gmail.com>
2021-04-27 15:15:26 +0200

    update block configs

--- src/main/kotlin/mods/betterfoliage/config/BlockConfig.kt
+++ src/main/kotlin/mods/betterfoliage/config/BlockConfig.kt
@@ -14,15 +14,15 @@ class BlockConfig  {
     val leafModels = models("leaves_models_default.cfg")
     val grassBlocks = blocks("grass_blocks_default.cfg")
     val grassModels = models("grass_models_default.cfg")
-    val mycelium = blocks("mycelium_blocks_default.cfg")
-    //    val dirt = blocks("dirt_default.cfg")
-    val crops = blocks("crop_default.cfg")
+//    val mycelium = blocks("mycelium_blocks_default.cfg")
+//    val dirt = blocks("dirt_default.cfg")
+//    val crops = blocks("crop_default.cfg")
     val logBlocks = blocks("log_blocks_default.cfg")
     val logModels = models("log_models_default.cfg")
-    val sand = blocks("sand_default.cfg")
-    val lilypad = blocks("lilypad_default.cfg")
-    val cactus = blocks("cactus_default.cfg")
-    val netherrack = blocks("netherrack_blocks_default.cfg")
+//    val sand = blocks("sand_default.cfg")
+//    val lilypad = blocks("lilypad_default.cfg")
+//    val cactus = blocks("cactus_default.cfg")
+//    val netherrack = blocks("netherrack_blocks_default.cfg")
 
     private fun blocks(cfgName: String) = ConfigurableBlockMatcher(BetterFoliage.logDetail, Identifier(BetterFoliage.MOD_ID, cfgName)).apply { list.add(this) }
     private fun models(cfgName: String) = ModelTextureListConfiguration(BetterFoliage.logDetail, Identifier(BetterFoliage.MOD_ID, cfgName)).apply { list.add(this) }
